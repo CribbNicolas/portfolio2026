5c0783000679203e811b6a9dcfad325fd9436a6f
Nicolas Cribb <cribbnicolas26@gmail.com>
2026-08-18 14:46:32 -0300

    fix(content): remover import muerto de yearsOfExperience en format.ts

--- content/schema/format.ts
+++ content/schema/format.ts
@@ -10,7 +10,6 @@
  */
 
 import type { Role, YearMonth } from "./content-schema";
-import { yearsOfExperience } from "./dates";
 
 // ---------------------------------------------------------------------------
 // Fechas
